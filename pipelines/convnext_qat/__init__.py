ce574c98357c57b6c7e607557f06986060d1d3ca
SyThang <vst221105@gmail.com>
2026-07-22 16:27:32 +0700

    Fix legacy pipeline import cycle

--- pipelines/convnext_qat/__init__.py
+++ pipelines/convnext_qat/__init__.py
@@ -4,8 +4,30 @@ Active development should target ``pipelines.fasterrcnn_qat``.
 This package remains only to keep older scripts and notebooks running.
 """
 
-from .models import build_fasterrcnn_convnext
-from .quantization import convert_selective_qat, prepare_selective_qat, set_qat_phase
+
+def build_fasterrcnn_convnext(*args, **kwargs):
+    from ..fasterrcnn_qat.models import build_fasterrcnn_convnext as impl
+
+    return impl(*args, **kwargs)
+
+
+def prepare_selective_qat(*args, **kwargs):
+    from .quantization import prepare_selective_qat as impl
+
+    return impl(*args, **kwargs)
+
+
+def convert_selective_qat(*args, **kwargs):
+    from .quantization import convert_selective_qat as impl
+
+    return impl(*args, **kwargs)
+
+
+def set_qat_phase(*args, **kwargs):
+    from .quantization import set_qat_phase as impl
+
+    return impl(*args, **kwargs)
+
 
 __all__ = [
     "build_fasterrcnn_convnext",
--- pipelines/convnext_qat/models/__init__.py
+++ pipelines/convnext_qat/models/__init__.py
@@ -1,1 +1,27 @@
-from ...fasterrcnn_qat.models import *  # noqa: F401,F403
+def build_fasterrcnn_convnext(*args, **kwargs):
+    from ...fasterrcnn_qat.models import build_fasterrcnn_convnext as impl
+
+    return impl(*args, **kwargs)
+
+
+def build_fasterrcnn_model(*args, **kwargs):
+    from ...fasterrcnn_qat.models import build_fasterrcnn_model as impl
+
+    return impl(*args, **kwargs)
+
+
+from ...fasterrcnn_qat.models.backbones import (  # noqa: E402
+    ConvNeXtFPNBackbone,
+    ResNetFPNBackbone,
+    build_convnext_fpn_backbone,
+    build_fpn_backbone,
+)
+
+__all__ = [
+    "build_fasterrcnn_convnext",
+    "build_fasterrcnn_model",
+    "build_fpn_backbone",
+    "build_convnext_fpn_backbone",
+    "ConvNeXtFPNBackbone",
+    "ResNetFPNBackbone",
+]
