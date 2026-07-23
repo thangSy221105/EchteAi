e4cdd16b41a072c73ec9c04f3c5f8b5d7a03fbb1
SyThang <vst221105@gmail.com>
2026-07-23 14:51:34 +0700

    Fix hybrid metrics compatibility wrapper

--- pipelines/fasterrcnn_qat/metrics.py
+++ pipelines/fasterrcnn_qat/metrics.py
@@ -1,1 +1,15 @@
-from ..convnext_qat.metrics import *  # noqa: F401,F403
+from ..convnext_qat.metrics import (  # noqa: F401
+    _coco_metrics,
+    evaluate_model,
+    native_detection_metrics,
+    rpn_recall,
+    save_metrics,
+)
+
+__all__ = [
+    "_coco_metrics",
+    "native_detection_metrics",
+    "rpn_recall",
+    "evaluate_model",
+    "save_metrics",
+]
